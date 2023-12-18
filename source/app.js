ad399a9ebc8d9e30e179894494d80746d4f36ec8
Nikolay <131699331+NikolDev@users.noreply.github.com>
2023-12-18 18:13:00 +0300

    Add files via upload

---
+++ source/app.js
@@ -0,0 +1,39 @@
+const newYear = "1/1/2024";
+
+const daysEl = document.querySelector(".day");
+const hourEl = document.querySelector(".hour");
+const minuteEl = document.querySelector(".minute");
+const secondEl = document.querySelector(".seconds");
+
+function timeCountDown() {
+  const newDate = new Date();
+  const newYearDate = new Date(newYear);
+  const totalSeconds = (newYearDate - newDate) / 1000;
+  const days = Math.floor(totalSeconds / 3600 / 24);
+  const hours = Math.floor(totalSeconds / 3600) % 24;
+  const minutes = Math.floor(totalSeconds / 60) % 60;
+  const seconds = Math.floor(totalSeconds) % 60;
+
+  daysEl.textContent = formatTime(days);
+  hourEl.textContent = formatTime(hours);
+  minuteEl.textContent = formatTime(minutes);
+  secondEl.textContent = formatTime(seconds);
+}
+
+function formatTime(time) {
+  return time >= 10 ? time : `0${time}`;
+}
+timeCountDown();
+setInterval(timeCountDown, 1000);
+
+const wrapper = document.querySelector('.wrapperSub');
+const btnPopup = document.querySelector('.btn-buy');
+const iconClose = document.querySelector('.icon-close');
+
+btnPopup.addEventListener('click', ()=> {
+    wrapper.classList.add('active-popup');
+});
+
+iconClose.addEventListener('click', ()=> {
+    wrapper.classList.remove('active-popup');
+});
