e3d6ac2ff00993595abca1ccb6e17a4e630d9c3f
Nikolay <131699331+NikolDev@users.noreply.github.com>
2024-01-13 20:36:59 +0300

    Add files via upload

---
+++ source/app.js
@@ -0,0 +1,9 @@
+/*document.querySelectorAll('a[href^="#"]').forEach(anchor =&gt; {
+  anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+      });
+  });
+});*/
