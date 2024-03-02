2a1cfa4711212ebd072bc9c53e279075b4d4b378
Nikolay <131699331+NikolDev@users.noreply.github.com>
2024-03-02 17:29:12 +0300

    Update app.js

--- source/app.js
+++ source/app.js
@@ -7,3 +7,8 @@
       });
   });
 });*/
+
+document.querySelector(".hamburger").addEventListener("click", function () {
+  document.querySelector(".header").classList.toggle("mobile");
+  document.querySelector("body").classList.toggle("hidden");
+});
