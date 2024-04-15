4a5d7fb281b6a24c1f0a91398099e85e5121f7d4
Nikolay Bondarchuk <bondanikei@gmail.com>
2024-04-15 16:29:11 +0300

    Time_window

---
+++ source/test.js
@@ -0,0 +1,18 @@
+/*let count = '0';
+let close = document.querySelector('.test')
+let closeBtn = document.querySelector('.btn')
+
+closeBtn.onclick = function () {
+  count++;
+  close.style.opacity = "0";
+  close.style.visibility = "hidden";
+  localStorage.setItem('count', count);
+}
+
+if (localStorage.count == 0) {
+  close.style.opacity = "1";
+  close.style.visibility = "visible";
+} else {
+  close.style.opacity = "0";
+  close.style.visibility = "hidden";
+}*/
